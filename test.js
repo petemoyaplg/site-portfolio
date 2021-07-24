d8523821c15ce98a3e2ec713a7cdcfabde926137
petemoyaplg <gaspardplg11@gmail.com>
2021-07-24 13:55:15 +0100

    maquette

---
+++ test.js
@@ -0,0 +1,8 @@
+const commentairefr = "J'ai de l'expérience dans une grande variété de technologies tout au long de ma carrière. Ma passion et mon objectif sont le développement d'infrastructures et d'applications qui stimulent l'analyse des données et l'automatisation des processus."
+const aProposfr = "Je suis un développeur Web Full Stack, J'aime concevoir et créer, autant que j'aime trouver des solutions à des problèmes difficiles et maximiser l'efficacité équilibrée avec élégance. Pas de problème trop petit ou trop grand, j'adore les défies."
+const aProposEn = "I am a front end developer, I love to design and create, as much as I love finding solutions to difficult problems and maximizing efficiency balanced with elegance. No problem too small or too big, I love challenges."
+const commentaireEn = "I have experience in a wide variety of technologies throughout my career. My passion and my goal are the development of infrastructures and applications that stimulate data analysis and process automation."
+console.log( aProposEn.length );
+
+
+
